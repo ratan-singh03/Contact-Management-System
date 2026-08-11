402dc455cb5cde0c5a12751f205986be84d12dbc
ratan-singh03 <ratanparsola@gmail.com>
2026-08-11 12:00:45 +0100

    Create Contact.java

---
+++ Contact.java
@@ -0,0 +1,62 @@
+package com.codsoft.contactmanagement.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "contacts")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false, length = 2000)
+    private String message;
+
+    public Contact() {
+    }
+
+    public Contact(String name, String email, String message) {
+        this.name = name;
+        this.email = email;
+        this.message = message;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
